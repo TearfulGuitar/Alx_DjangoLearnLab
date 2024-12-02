95dc10c835f6caa1f2004f2f0a4e64debea4c755
TearfulGuitar <robert254joe@gmail.com>
2024-12-02 13:22:34 +0300

    Introduction to Building APIs with Django REST Framework

--- api_project/api_project/urls.py
+++ api_project/api_project/urls.py
@@ -16,23 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from api.views import BookList, BookViewSet
-from rest_framework.routers import DefaultRouter
-from rest_framework.authtoken.views import obtain_auth_token
-
-router = DefaultRouter() # Create an Instance of DefaultRouter Class
-router.register(r'books_all', BookViewSet, basename='book_all')
-
 
 urlpatterns = [
-    # Route for the BookList view (ListAPIView)
-    path('books/', BookList.as_view(), name='book-list'), # Maps to the BookList view
-
-    # Include the router URLs for BookViewSet (all CRUD operations)
-    path('', include(router.urls)),  # This includes all routes registered with the router
-    
-    # path to obtain auth token
-    path('api-token-auth', obtain_auth_token),
+    path('admin/', admin.site.urls),
+    path('api/', include('api.urls')),
 ]
-
 
